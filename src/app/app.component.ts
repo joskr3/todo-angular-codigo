491a98de8e3c8ec39f7737778b6a47d46750df42
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-16 21:45:42 -0500

    proyecto de tareas  , angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Tarea } from "./Tarea";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "App",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'demo-app-todolist';
+  tareas: Tarea[] = [];
+  nombreNuevaTarea: string = "";
+
+  guardarTareas() {
+    if (this.nombreNuevaTarea) {
+      let tarea = new Tarea();
+      tarea.nombre = this.nombreNuevaTarea;
+      tarea.completada = true;
+      this.tareas.push(tarea);
+    } else {
+      alert("Debe ingresar un nombre para la tarea");
+    }
+  }
+
+  marcarTareaComoTerminada(id: number) {
+    this.tareas[id].completada = !this.tareas[id].completada;
+  }
+
+  eliminarTarea(id: number) {
+    // this.tareas.splice(id, 1);
+    this.tareas = this.tareas.filter((_, index) => index !== id);
+  }
 }
